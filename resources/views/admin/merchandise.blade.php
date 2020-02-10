675d87d9e8e27510dc689c92268ab994b638a469
Akbarsn <new.akbarsatya@gmail.com>
2020-02-10 23:47:49 +0700

    feat: Adding layouts

---
+++ resources/views/admin/merchandise.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+
+@section('body')
+    {{-- Isi sini gan --}}
+@endsection
---
+++ resources/views/page/berita-detail.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.apps')
+
+@section('body')
+    {{-- Isi sini gan --}}
+@endsection

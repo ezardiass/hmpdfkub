82b04edf230edb084bb92609db3fd43ffca4010a
Akbarsn <new.akbarsatya@gmail.com>
2020-02-11 10:29:47 +0700

    fix: Adding link for fontawesome

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,6 +15,7 @@
     @yield('body')
     @include('inc.footer')
 </body>
+<script src="https://kit.fontawesome.com/5a91d5d98c.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>

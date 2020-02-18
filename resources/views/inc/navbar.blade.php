e9b7bcacefd6bfccb25de9c982a5cdeeaeb15e59
Akbarsn <new.akbarsatya@gmail.com>
2020-02-18 08:04:18 +0700

    [Akbar] feat: Adding navbar and its style

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="{{asset("css/nav.css")}}">
+
+<nav class="navbar fixed-top navbar-expand-md">
+    <span class="navbar-brand">HMPD</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_hmpd"
+        aria-controls="navbar_hmpd" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar_hmpd">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <span class="my-4 my-md-0 list">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profil</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Bidang Kerja</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown04">
+                        <a class="dropdown-item" href="#">Bidang 1</a>
+                        <a class="dropdown-item" href="#">Bidang 2</a>
+                        <a class="dropdown-item" href="#">Bidang 3</a>
+                        <a class="dropdown-item" href="#">Bidang 4</a>
+                        <a class="dropdown-item" href="#">Bidang 5</a>
+                        <a class="dropdown-item" href="#">Bidang 6</a>
+                        <a class="dropdown-item" href="#">Bidang 7</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Aktifitas</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown04">
+                        <a class="dropdown-item" href="#">Berita</a>
+                        <a class="dropdown-item" href="#">Galeri</a>
+                        <a class="dropdown-item" href="#">Merchandise</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Link</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown04">
+                        <a class="dropdown-item" href="#">Podcast</a>
+                        <a class="dropdown-item" href="#">Sambatkuy</a>
+                    </div>
+                </li>
+            </ul>
+        </span>
+    </div>
+</nav>
--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -7,15 +7,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HMPD FKUB</title>
 </head>
+<link rel="stylesheet" href="{{asset("css/root.css")}}">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-@include('inc.navbar')
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 
 <body>
+    @include('inc.navbar')
     @yield('body')
     @include('inc.footer')
 </body>
 <script src="https://kit.fontawesome.com/5a91d5d98c.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 

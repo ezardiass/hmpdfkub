ee6d9a0ab93181a8d9675fe222fc3d688516040e
Akbarsn <new.akbarsatya@gmail.com>
2020-02-18 23:01:50 +0700

    [Akbar] feat: Adding footer

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" href="{{asset("css/footer.css")}}">
+<footer>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="footer-image center">
+                <img src="{{asset("image/hmpd.png")}}" id="img-footer" alt="Logo HMPD">
+            </div>
+            <div class="container">
+                <div class="d-flex justify-content-around">
+                    <div class="col">
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                    </div>
+                    <div class="col">
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </div>
+                    <div class="col">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="center">
+                hmpdfkub@gmail.com
+            </div>
+        </div>
+        <div class="col-md-5">
+            <div class="footer-head">
+                Kontak Kami
+            </div>
+            <div class="footer-isi">
+                Griya Mahasiswa Pendidikan Dokter<br>
+                Gedung Pembelajaran Bersama<br>
+                Jl. Veteran No. 8<br>
+                Malang 65145<br>
+                Indonesia
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="footer-head">
+                Menu
+            </div>
+            <div class="footer-isi">
+                <a href="#">Database Mahasiswa</a><br>
+                <a href="#">Profil Fakultas Kedokteran</a><br>
+                <a href="#">Kontak Kami</a><br>
+            </div>
+        </div>
+    </div>
+</footer>

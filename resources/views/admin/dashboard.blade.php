e59267efa26e3ac2b2fe8dcef4d81206d62ed58f
Akbarsn <new.akbarsatya@gmail.com>
2020-02-28 21:33:28 +0700

    Fixing dashboard pages

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,48 +2,65 @@
 
 @section('body')
 <link rel="stylesheet" href="{{asset("css/dashboard.css")}}">
-    <div class="container" style="margin-top:72px">
-        <div >
-            <h2 class="judul">DASHBOARD</h2>
+<div class="container" style="margin-top:72px">
+    <div class="header">
+        <h2 class="judul">DASHBOARD</h2>
+    </div>
+    <div class="d-flex justify-content-around flex-wrap">
+        <div class="card">
+            <a href="{{url('/admin/berita')}}">
+                <img src="{{asset("images/win.jpg")}}" class="card-img-top">
+                <div class="card-body">
+                    <div class="center">
+                        <h5 class="card-title">Berita</h5>
+                    </div>
+                </div>
+            </a>
         </div>
-        <div class="row">
-            <div class="col-4" id="content-col">
-                <div class="content-isi">
-                <img src="{{asset("images/win.jpg")}}" alt="" class="foto">
-                <h4 class="sub-judul">BERITA</h4>
+
+        <div class="card">
+            <a href="{{url('/admin/event')}}">
+                <img src="{{asset("images/event.jpg")}}" class="card-img-top">
+                <div class="card-body">
+                    <div class="center">
+                        <h5 class="card-title">Event</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="col-4" id="content-col">
-                <div class="content-isi">
-                <img src="{{asset("images/event.jpg")}}" alt="" class="foto">
-                <h4 class="sub-judul">EVENT</h4>
-                </div>
-            </div>
-            <div class="col-4" id="content-col">
-                <div class="content-isi">
-                <img src="{{asset("images/keychain.jpg")}}" alt="" class="foto">
-                <h4 class="sub-judul">MERCH</h4>
-                </div>
-            </div>
+            </a>
         </div>
-        <div class="row">
-            <div class="col-4" id="content-col">
-                <div class="content-isi">
-                <img src="{{asset("images/proker.jpg")}}" alt="" class="foto">
-                <h4 class="sub-judul">PROKER</h4>
+
+        <div class="card">
+            <a href="{{url('/admin/merchandise')}}">
+                <img src="{{asset("images/keychain.jpg")}}" class="card-img-top">
+                <div class="card-body">
+                    <div class="center">
+                        <h5 class="card-title">Merchandise</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="col-4" id="content-col">
-                <div class="content-isi">
-                <img src="{{asset("images/gallery.jpg")}}" alt="" class="foto">
-                <h4 class="sub-judul">GALERI</h4>
+            </a>
+        </div>
+
+        <div class="card">
+            <a href="{{url('/admin/proker')}}">
+                <img src="{{asset("images/proker.jpg")}}" class="card-img-top">
+                <div class="card-body">
+                    <div class="center">
+                        <h5 class="card-title">Proker</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="col-4" id="content-col">
-                <div >
-                <img src="{{asset("images/")}}" alt="" class="foto">
+            </a>
+        </div>
+
+        <div class="card">
+            <a href="{{url('/admin/galeri')}}">
+                <img src="{{asset("images/gallery.jpg")}}" class="card-img-top">
+                <div class="card-body">
+                    <div class="center">
+                        <h5 class="card-title">Galeri</h5>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
+</div>
 @endsection

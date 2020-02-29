6d9f0af8a02c7b491bd0a7c7f3aeb1e7785bbfcd
Akbarsn <new.akbarsatya@gmail.com>
2020-03-01 00:10:24 +0700

    Binding home and bidang kerja

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,17 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Event;
 use App\BidangKerja;
-use App\Merchandises;
+use App\Proker;
+use App\News;
 
 class HomeController extends Controller
 {
-    public function GetHomeView(){
+    public function GetHomeView()
+    {
         $event = Event::where('isDone', false)->orderBy('created_at', 'desc')->limit(6)->get();
-        $merch = Merchandises::orderBy('created_at', 'desc')->limit(6)->get();
+        $news = News::orderBy('created_at', 'desc')->limit(6)->get();
         $bidangKerja = BidangKerja::all();
         return view('page.home')
             ->with('event', $event)
-            ->with('merch', $merch)
+            ->with('news', $news)
             ->with('bidangKerja', $bidangKerja);
+    }
+
+    public function GetBidangKerja(Request $request, $id)
+    {
+        $bidangKerja = BidangKerja::find($id);
+        $proker = Proker::where('bidang_id', $id)->get();
+        return view('page.bidang-kerja')->with('bidangKerja', $bidangKerja)->with('proker', $proker);
     }
 }
